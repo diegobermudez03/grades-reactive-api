00a64d1c68e10874cf74738d56bcaf567a9087b7
Diego Bermudez <bermudezjimenezjuandiego@gmail.com>
2024-11-02 00:34:07 -0500

    first commit

---
+++ src/main/java/com/diegobermudez/taller_reactivo/TallerReactivoApplication.java
@@ -0,0 +1,13 @@
+package com.diegobermudez.taller_reactivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerReactivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerReactivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diegobermudez/taller_reactivo/TallerReactivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegobermudez.taller_reactivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerReactivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
